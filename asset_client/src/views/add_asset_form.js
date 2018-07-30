8cc0ecd5c036c44243425286195f3a155063b319
dexidlt <40951555+dexidlt@users.noreply.github.com>
2018-07-29 23:01:47 -0400

    Update add_asset_form.js
    update authorize reporters note again.

--- asset_client/src/views/add_asset_form.js
+++ asset_client/src/views/add_asset_form.js
@@ -96,7 +96,7 @@ const AddAssetForm = {
         ]),
 
         m('.reporters.form-group',
-          m('label', 'Authorize Reporters (people who can update the tracking info of an asset)'),
+          m('label', 'Authorize Reporters (People who can update the tracking info of an asset. Use the drop-down to the right to select what they can update.)'),
           vnode.state.reporters.map((reporter, i) =>
             m('.row.mb-2',
               m('.col-sm-8',
