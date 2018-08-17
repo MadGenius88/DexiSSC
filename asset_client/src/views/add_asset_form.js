f00195c88eeede67a7784bce870edcf344fb44d2
dexidlt <40951555+dexidlt@users.noreply.github.com>
2018-08-17 08:41:34 -0400

    Update add_asset_form.js
    added verbiage to authorize reporters.

--- asset_client/src/views/add_asset_form.js
+++ asset_client/src/views/add_asset_form.js
@@ -96,7 +96,7 @@ const AddAssetForm = {
         ]),
 
         m('.reporters.form-group',
-          m('label', 'Authorize Reporters'),
+          m('label', 'Authorize Reporters. Reporters can update the tracking info of an asset. Use the drop-down to the right to select what they can update.'),
           vnode.state.reporters.map((reporter, i) =>
             m('.row.mb-2',
               m('.col-sm-8',
